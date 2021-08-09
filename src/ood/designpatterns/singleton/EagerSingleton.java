f3945f2a77bde2639b0f87c7db56b727a21e6456
Chao Tong <tongchao0428@hotmail.com>
2021-08-09 11:43:48 +0800

    Singleton practice

--- src/ood/designpatterns/singleton/EagerSingleton.java
+++ src/ood/designpatterns/singleton/EagerSingleton.java
@@ -1,10 +1,14 @@
 package ood.designpatterns.singleton;
 
 public class EagerSingleton {
+    // 若没有static则这个instance需要依赖于一个object而存在
     private static final EagerSingleton INSTANCE = new EagerSingleton();
 
-    private EagerSingleton(){}
+    private EagerSingleton() {
+        // default constructor is public
+    }
 
+    // 必须是static
     public static EagerSingleton getInstance() {
         return INSTANCE;
     }
--- src/ood/designpatterns/singleton/LazySingleton.java
+++ src/ood/designpatterns/singleton/LazySingleton.java
@@ -1,14 +1,21 @@
 package ood.designpatterns.singleton;
 
+/**
+ * Lazy version, 需要加锁, 不然有并发问题
+ */
 public final class LazySingleton {
-    private static volatile LazySingleton instance = null; // 若没有static则这个instance需要依赖于一个object而存在
-    private LazySingleton() { // default constructor is public
+    // 若没有static则这个instance需要依赖于一个object而存在
+    private static volatile LazySingleton instance = null; // no final here
+
+    private LazySingleton() {
+        // default constructor is public
     }
+
     // 必须是static
     public static LazySingleton getInstance() {
         if (instance == null) {
             synchronized (LazySingleton.class) {
-                // when more than two threads run into the first null check same time
+                // when more than two threads run into the first null check at the same time
                 // to avoid instanced more than one time, it needs to be checked again.
                 if (instance == null) {
                     instance = new LazySingleton();

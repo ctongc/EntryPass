ef88456a6f1fdc4caa78ad0f6fb50d171b6b56e4
Chao Tong <tongchao0428@hotmail.com>
2021-08-16 20:09:32 +0800

    Enum, Nested Class and iterator practice

---
+++ src/practice/Enums.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Enums {
+    public enum RainbowColor { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE }
+
+    public static void main(String[] args) {
+        RainbowColor color = RainbowColor.BLUE;
+        for (RainbowColor c : RainbowColor.values()) {
+            System.out.println(c);
+            System.out.println(c.ordinal());
+        }
+        System.out.println(RainbowColor.valueOf("RED"));
+        System.out.println(RainbowColor.valueOf("red"));
+    }
+}
---
+++ src/practice/IteratorPractice.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class IteratorPractice {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for(ListIterator<Integer> it = list.listIterator(); it.hasNext();) {
+            System.out.println("One step forward: " + it.next());
+            if (it.hasNext()) {
+                System.out.println("Another step forward: " + it.next());
+                System.out.println("One step backward: " + it.previous());
+            }
+        }
+    }
+}

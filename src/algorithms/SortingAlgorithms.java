e94603b1f84d3b8e82cc44d1feb0e8695fad9d9d
Chao Tong <tongchao0428@hotmail.com>
2021-09-02 21:24:15 +0800

    Adding Topological sort of GraphSearchIII to sorting algors

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -1,6 +1,9 @@
 package algorithms;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
 
 public class SortingAlgorithms {
 
@@ -209,6 +212,46 @@ public class SortingAlgorithms {
                 swap(array, one--, zero);
             }
         }
+    }
+
+    /**
+     * Topological Sort
+     * Given n courses and the prerequisites of each course
+     * find a valid order to take all the courses (aka topological order)
+     *
+     * Time = O(n + e)
+     * Space = O(n)
+     */
+    public int[] topologicalSort(List<List<Integer>> graph) {
+        int numCourses = graph.size();
+        int[] topologicalOrder = new int[numCourses];
+        int[] incomingEdges = new int[numCourses];
+        for (int x = 0; x < numCourses; x++) {
+            for (int y : graph.get(x)) {
+                incomingEdges[y]++;
+            }
+        }
+
+        // nodes with no incoming edges
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int x = 0; x < numCourses; x++) {
+            if(incomingEdges[x] == 0) {
+                queue.offer(x);
+            }
+        }
+
+        int numExpanded = 0;
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            topologicalOrder[numExpanded++] = x;
+            for (int y : graph.get(x)) {
+                if (--incomingEdges[y] == 0) {
+                    queue.offer(y);
+                }
+            }
+        }
+
+        return numExpanded == numCourses ? topologicalOrder : new int[]{};
     }
 
     /**

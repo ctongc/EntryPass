8270d0fc43d647a1e7b37f3a539ce688b20c8194
Chao Tong <tongchao0428@hotmail.com>
2022-04-05 20:29:28 +0800

    Heap implementation

--- src/datastructures/MinHeap.java
+++ src/datastructures/MinHeap.java
@@ -5,19 +5,18 @@ import java.util.NoSuchElementException;
 
 /**
  * An example implementation of capacity limited min heap containing only int values,
- * with the capacity to do update and poll at a specific index.
- *
- * This is for demonstration of percolateUp and percolateDown methods and
- * how to utilize these methods to do basic heap operations.
+ * with the capacity to do update and poll at a specific index. This is for demonstration
+ * of percolateUp and percolateDown methods and how to utilize these methods to do basic
+ * heap operations.
  *
  * The public methods provided are:
- * size()
- * isEmpty()
- * isFull()
- * peek()
- * offer(int ele)
- * poll()
- * update(int index, int value) - update the element at index to a given new value
+ * - size()
+ * - isEmpty()
+ * - isFull()
+ * - peek()
+ * - offer(int ele)
+ * - poll()
+ * - update(int index, int value) // update the element at index to a given new value
  */
 public class MinHeap {
     private int[] array;
@@ -27,6 +26,7 @@ public class MinHeap {
         if (array == null || array.length == 0) {
             throw new IllegalArgumentException("Input array can not be null or empty.");
         }
+
         this.array = array;
         size = array.length;
         heapify();
@@ -36,6 +36,7 @@ public class MinHeap {
         if (cap <= 0) {
             throw new IllegalArgumentException("Capacity can not be <= 0.");
         }
+
         array = new int[cap];
         size = 0;
     }
@@ -56,6 +57,7 @@ public class MinHeap {
         if (isEmpty()) {
             throw new NoSuchElementException("Heap is empty.");
         }
+
         return array[0];
     }
 
@@ -63,13 +65,17 @@ public class MinHeap {
         if (isEmpty()) {
             throw new NoSuchElementException("Heap is empty");
         }
+
         int result = array[0];
         array[0] = array[size - 1];
         size--;
         percolateDown(0);
+
+        // check if we need to resize
         if (size < array.length / 4) {
             array = Arrays.copyOf(array, array.length / 2);
         }
+
         return result;
     }
 
@@ -77,15 +83,10 @@ public class MinHeap {
         if (isFull()) {
             array = Arrays.copyOf(array, (int) (array.length * 1.5));
         }
-        array[size] = ele;
-        percolateUp(size);
-        size++;
-    }
 
-    private void heapify() {
-        for (int i = size / 2 - 1; i >= 0; i--) {
-            percolateDown(i);
-        }
+        array[size] = ele;
+        size++;
+        percolateUp(size - 1); // index of last ele, it's new size - 1
     }
 
     private void percolateUp(int index) {
@@ -108,7 +109,6 @@ public class MinHeap {
             // use swapCandidate to indicate the smaller element between left and right child
             int swapCandidate = rightChildIndex <= size - 1 && array[leftChildIndex] > array[rightChildIndex] ?
                     rightChildIndex : leftChildIndex;
-
             if (array[index] > array[swapCandidate]) {
                 swap(index, leftChildIndex);
             } else {
@@ -118,10 +118,23 @@ public class MinHeap {
         }
     }
 
+    /**
+     * Last node index = n - 1
+     * So last index's parent = ((n - 1) - 1) / 2 = n / 2 - 1
+     */
+    private void heapify() {
+        // 对array的前一半，从后往前建堆
+        // 从底向上来完成，因为要保证heapify()每一个节点的时候，它的左右子树都已经是个堆了
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            percolateDown(i);
+        }
+    }
+
     public void update(int index, int ele) {
         if (index < 0 || index > size - 1) {
             throw new ArrayIndexOutOfBoundsException("Invalid index: " + index);
         }
+
         int oldValue = array[index];
         array[index] = ele;
         if (oldValue < ele) {
